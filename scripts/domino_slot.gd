6416ffc59d7cb6fd65e2d37ee753bb273e4e8fb6
Lenny <mightylenny5@gmail.com>
2026-03-04 23:17:28 -0500

    Remove input event in domino slot

--- scripts/domino_slot.gd
+++ scripts/domino_slot.gd
@@ -2,7 +2,3 @@ extends Area2D
 
 var dir
 var is_head: bool
-
-func _on_input_event(_viewport, event, _shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		slot_clicked.emit(get_parent())
