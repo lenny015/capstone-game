d59f75b2286398783b60b080fe616fde38041f2a
Lenny <mightylenny5@gmail.com>
2026-03-18 18:15:43 -0400

    Add multiplayer button, does nothing rn

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,5 +3,9 @@ extends Control
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/game_board.tscn")
 
+func _on_multiplayer_pressed():
+	# Change to lobby scene
+	pass
+
 func _on_exit_pressed():
 	get_tree().quit()
