022b1ff2d3e8faf84a7055f1d3537215789ca044
Lenny <mightylenny5@gmail.com>
2026-04-10 01:25:31 -0400

    Fix resolution change to apply on current monitor

--- scripts/settings_autoload.gd
+++ scripts/settings_autoload.gd
@@ -9,10 +9,13 @@ const RESOLUTIONS: Array[Vector2i] = [
 
 func apply_windowed(res_index: int) -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	DisplayServer.window_set_size(RESOLUTIONS[res_index])
-	var screen_size := DisplayServer.screen_get_size()
-	var window_size := DisplayServer.window_get_size()
-	DisplayServer.window_set_position((screen_size - window_size) / 2)
+	var res := RESOLUTIONS[res_index]
+	var current_screen := DisplayServer.window_get_current_screen()
+	var screen_pos := DisplayServer.screen_get_position(current_screen)
+	var screen_size := DisplayServer.screen_get_size(current_screen)
+	var centered_pos := screen_pos + (screen_size - res) / 2
+	DisplayServer.window_set_size(res)
+	DisplayServer.window_set_position(centered_pos)
 
 func apply_fullscreen() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
